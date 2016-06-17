ad4c4a9007ffddecef0b01c1ce50cdc6d7afd432
Rob Ortiz <rob@dragonwrench.com>
2016-06-17 18:39:09 -0500

    switching site urls for omni to connect with graph for this gem's strategy

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -6,9 +6,9 @@ module OmniAuth
       option :name, :office365
 
       option :client_options, {
-        site:          'https://outlook.office365.com/',
-        token_url:     'https://login.windows.net/common/oauth2/token',
-        authorize_url: 'https://login.windows.net/common/oauth2/authorize'
+        site:          'https://login.microsoftonline.com/common/oauth2/authorize',
+        token_url:     'https://graph.microsoft.com/common/oauth2/token',
+        authorize_url: 'https://graph.microsoft.com/common/oauth2/authorize'
       }
 
       option :authorize_params, {
